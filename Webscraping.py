254bdc4111e2fa87ed0f6b1736870bd5365c0235
Pablo Castrejón <pablocs117@gmail.com>
2023-03-14 11:45:17 -0600

    Update Webscraping.py

--- Webscraping.py
+++ Webscraping.py
@@ -71,7 +71,7 @@ def webscraping():
                 try:
                     results[i].append(lxml_soup.xpath(xpath_of_columns[j])[0])
                 except:
-                    results[i].append('NAN')
+                    results[i].append('')
                 iaItemListWithLink[names_of_columns[j]][iaItemListWithLink['ItemUPC']==i] = results[i][j]
         except:
             for j in range(0,number_of_columns):
@@ -79,7 +79,7 @@ def webscraping():
                 try:
                     results[i].append(lxml_soup.xpath(xpath_of_columns[j])[0])
                 except:
-                    results[i].append('NAN')
+                    results[i].append('')
                 iaItemListWithLink[names_of_columns[j]][iaItemListWithLink['ItemUPC']==i] = results[i][j]
         # Funciones del contador
         time_of_exec = round(time.time(),0) - round(start_time,0)
